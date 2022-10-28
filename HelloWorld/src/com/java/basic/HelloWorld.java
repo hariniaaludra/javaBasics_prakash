485bf5376f0fdaf644339671c39fb1fe55d51f8f
unknown <prakash308ece@gmail.com>
2022-10-28 19:46:47 +0530

    helloworld

--- HelloWorld/src/com/java/basic/HelloWorld.java
+++ HelloWorld/src/com/java/basic/HelloWorld.java
@@ -6,7 +6,26 @@ public class HelloWorld {
 	   {
 		System.out.println(5+"*"+i+"="+5*i);   
 	   }
-	   
    }
+   
+	   void print(int n){
+		   
+	   for(int i=1;i<=10;i++) {
+		   System.out.println(n+"*"+i+"="+n*i);
+	   }
+		   
+		   }
+	 void print(int n,int from ,int to)  {
+			 
+				 for(int i=from;i<=to;i++)
+				 {
+					 System.out.println(n+"*"+i+"="+n*i);
+				 }
+			 }
+			 
+	 
+	   }
 
-}
+
+
+  
--- HelloWorld/src/com/java/basic/multiplicationrun.java
+++ HelloWorld/src/com/java/basic/multiplicationrun.java
@@ -4,8 +4,9 @@ public class multiplicationrun {
 
 	public static void main(String[] args) {
     HelloWorld table=new HelloWorld();
-    table.print();
-
+   // table.print();
+    table.print(6);
+    table.print(6,34,65);
 	}
 
 }
