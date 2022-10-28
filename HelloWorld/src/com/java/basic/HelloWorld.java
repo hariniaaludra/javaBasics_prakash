145c70df45444f5639904ea72f9d349d2698d7d0
unknown <prakash308ece@gmail.com>
2022-10-28 18:24:52 +0530

    fist commit

---
+++ HelloWorld/src/com/java/basic/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.java.basic;
+
+public class HelloWorld {
+   void print(){
+	   for(int i=1;i<=10;i++)
+	   {
+		System.out.println(5+"*"+i+"="+5*i);   
+	   }
+	   
+   }
+
+}
---
+++ HelloWorld/src/com/java/basic/multiplicationrun.java
@@ -0,0 +1,11 @@
+package com.java.basic;
+
+public class multiplicationrun {
+
+	public static void main(String[] args) {
+    HelloWorld table=new HelloWorld();
+    table.print();
+
+	}
+
+}
