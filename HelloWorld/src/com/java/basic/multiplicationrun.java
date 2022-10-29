c0c61fe7be812f0adc4a16ed44ff78e4b86690a6
prakash.g@aaludra.com <prakash308ece@gmail.com>
2022-10-29 09:50:00 +0530

    File changed

--- HelloWorld/src/com/java/basic/multiplicationrun.java
+++ HelloWorld/src/com/java/basic/multiplicationrun.java
@@ -4,9 +4,9 @@ public class multiplicationrun {
 
 	public static void main(String[] args) {
     HelloWorld table=new HelloWorld();
-   // table.print();
-    table.print(6);
-    table.print(6,34,65);
+   table.print();
+    //table.print(6);
+    //table.print(6,34,65);
 	}
 
 }
