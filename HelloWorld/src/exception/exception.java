9be1d8f6c5b55ed12e8ab208cc5e90fe32990918
prakash.g@aaludra.com <prakash308ece@gmail.com>
2022-11-07 12:31:14 +0530

    javaexception

---
+++ HelloWorld/src/exception/exception.java
@@ -0,0 +1,5 @@
+package exception;
+
+public class exception extends Exception {
+
+}
---
+++ HelloWorld/src/exception/handling.java
@@ -0,0 +1,26 @@
+package exception;
+
+public class handling {
+	//private static char[] c;
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=15;
+		int c=0;
+		try {
+			 c=a+b;
+		}
+			catch(Exception e) {
+				System.err.println("error accured");
+			}
+				
+				System.out.println(c);
+				System.out.println("End of the program");
+				
+				
+	}
+	
+}
+	
+
+
