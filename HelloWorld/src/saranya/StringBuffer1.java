bc28617f0fb10303c0c67a1fc85e672fbe8b67da
prakash.g@aaludra.com <prakash308ece@gmail.com>
2022-11-26 18:56:17 +0530

    strings

---
+++ HelloWorld/src/saranya/StringBuffer1.java
@@ -0,0 +1,18 @@
+package saranya;
+public class StringBuffer1 {
+	public static void main(String[] args) {
+		StringBuffer c=new StringBuffer("Syghuj");
+		System.out.println(c.capacity());
+		
+		System.out.println(c.replace(1,3,"Java"));  
+		System.out.println(c.insert(1,"Java"));
+		System.out.println(c.append("Java"));
+		System.out.println(c.delete(1,3));
+		System.out.println(c.reverse());  
+	     c.ensureCapacity(100);
+		System.out.println(c.capacity());
+
+	
+	}
+
+}
--- HelloWorld/src/suriya/RectangularTest.java
+++ HelloWorld/src/suriya/RectangularTest.java
@@ -1,16 +1,12 @@
 package suriya;
 
-public class RectangularTest {
-	     
+class RectangularTest {
 
-	
-		
-			public static void main(String[] args) {
-				Rectangular ra=new Rectangular(2,2);
-		System.out.println(ra.getLength()*ra.getBreadth());
-	int cer=	ra.getLength()*ra.getBreadth();
+	public static void main(String[] args) {
+		Rectangular ra = new Rectangular(2, 2);
+		int cer = ra.getLength() * ra.getBreadth();
 		System.out.println(cer);
-		
+
 	}
 
 }
